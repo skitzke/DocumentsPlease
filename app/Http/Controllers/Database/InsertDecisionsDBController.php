6c73909b2363d2afc36258d9e8b7e2f7d1de38f0
skitzke <egslotz@gmail.com>
2022-04-12 19:46:15 +0200

    Created unique name in tables

--- app/Http/Controllers/Database/InsertDecisionsDBController.php
+++ app/Http/Controllers/Database/InsertDecisionsDBController.php
@@ -7,31 +7,32 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 
-class DecisionsDBController
+class InsertDecisionsDBController
 {
     //Making a function to insert the decision made in the game.
-    public function insertDecision($decision, $jail)
+    public function insertDecision($username, $decision, $jail)
     {
         //Making an array for the column names.
-        $columnName = array('decision', 'jail');
+        $columnName = array('Username','Verified', 'Jail');
 
         //If the table exists insert the given parameters into the database.
-        if (Schema::hasColumns('DocumentsPlease', $columnName))
+        if (Schema::hasColumns('Decision', $columnName))
         {
             //Insertion query for the database.
-            DB::insert('insert into DocumentsPlease (decision, jail) values (?, ?)', [$decision, $jail]);
+            DB::insert('insert into Decision (Username, Verified, Jail) values (?, ?, ?)', [$username, $decision, $jail]);
         }
         //If table does not exist.
         else
         {
             //Create a table with the given data type and names for the columns.
-            Schema::create('DocumentsPlease', function(Blueprint $table){
+            Schema::create('Decision', function(Blueprint $table){
                 $table->bigIncrements('id');
-                $table->string('decision');
-                $table->string('jail');
+                $table->string('Username');
+                $table->string('Verified');
+                $table->string('Jail');
             });
             //Then insert it after the table has been created.
-            DB::insert('insert into DocumentsPlease (decision, jail) values (?, ?)', [$decision, $jail]);
+            DB::insert('insert into Decision (Username, Verified, Jail) values (?, ?, ?)', [$username, $decision, $jail]);
         }
     }
 }
