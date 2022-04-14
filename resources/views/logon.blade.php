cc1e5eb7672f195f09f560f332c3ec880d5d0c76
skitzke <egslotz@gmail.com>
2022-04-14 18:51:31 +0200

    Added login css & login page

--- resources/views/logon.blade.php
+++ resources/views/logon.blade.php
@@ -5,18 +5,14 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/login.css') }}" />
         <title>Documents please Menu</title>
     </head>
     <body>
-        <canvas id="menuBackground">
-            <div id="container">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required>
-                <button type="submit">Login</button>
-            </div>
-        </canvas>
-
+        <div class="container">
+            <canvas id="menuBackground"></canvas>
+            <img id="rulebook" src="{{asset('images/RuleBook.png')}}" alt="rulebook">
+        </div>
     </body>
     <script type="text/javascript" src="{{URL::asset('js/app.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('js/canvas.js')}}"></script>
