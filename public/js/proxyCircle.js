4ef0a05017366930724e9a003763e65abeff64ca
skitzke <egslotz@gmail.com>
2022-04-15 19:01:29 +0200

    New images added

--- public/js/proxyCircle.js
+++ public/js/proxyCircle.js
@@ -1,8 +1,5 @@
-
-
+//Does not work yet, but the idea was to imitate the circle class so that this class can be used as a proxy to be called from html.
 class ProxyCircle extends Circle{
-
-
     proxyDrawShape(){
         return super.drawShape(context);
     }
@@ -13,5 +10,4 @@ class ProxyCircle extends Circle{
     proxyRulebookCircle(){
         return this.rulebookCircle.drawShape(context);
     }
-
 }
