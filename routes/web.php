c435328d2e94afb91c52cfaa8d20aa65c53d449e
Jonas_R <jramanauskas120@gmail.com>
2022-04-15 20:09:09 +0300

    minor update to routes
    User starts with log in page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('logon');
 });
+
+Route::get('home', function (){
+    return view('index');
+})->name('home');
+
